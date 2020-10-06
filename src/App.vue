f3318a6e6f2a6f7f09f7fd0e01a87e3e428929c8
soarless <353033712@qq.com>
2020-10-06 18:45:41 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <!-- <div class="nav"> -->
+      <ul>
+        <li><i class="fa fa-clock-o"></i>计划板</li>
+        <li>首页</li>
+        <li>asd</li>
+      </ul>
+    <!-- </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html,body,#app{
+  height:100%;
+  margin:0;
+  overflow:hidden;
+}
+.nav {
+  height:20%;
+  margin:0;
+}
+ul {
+  margin:0;
+  height:10%;
+  background-color:#F8F8F8;
+  border-bottom:1px solid #ECECEC;
+  display:flex;
+  align-items:center;
+}
+li {
+  display:inline;
+  font-size:1.3rem;
+  margin-right:1rem;
+}
+</style>
